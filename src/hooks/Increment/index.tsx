ceaa425a463faf519954dccc1cab412aed95453c
indrasatria <putuindrasatrianugraha@gmail.com>
2024-01-11 01:15:03 +0800

    first hook created

--- src/hooks/Increment/index.tsx
+++ src/hooks/Increment/index.tsx
@@ -7,9 +7,21 @@ const Increment = () => {
     setCount(count + 1);
   };
 
+  const handleClickMinus = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+
   return (
-    <div className=" flex h-48 flex-col items-center justify-center">
-      <label>{count}</label>
+    <div className=" flex h-48 items-center justify-center gap-8">
+      <button
+        onClick={() => handleClickMinus()}
+        className=" flex h-8 w-8 items-center justify-center bg-black text-white"
+      >
+        -
+      </button>
+      <label className="">{count}</label>
       <button
         onClick={() => handleClick()}
         className="flex h-8 w-8 items-center justify-center bg-black text-white"
